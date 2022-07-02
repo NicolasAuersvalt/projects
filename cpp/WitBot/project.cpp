08046ff70bea2c18f96ae9106ad5c8cd8a52e7b5
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2022-07-02 05:12:19 -0300

    Correções

--- cpp/WitBot/project.cpp
+++ cpp/WitBot/project.cpp
@@ -1,44 +1,39 @@
-/* Comment this out to disable prints and save space */
+// Configurações passadas pelo App Blynk
 #define BLYNK_TEMPLATE_ID "TMPLrG9kgao2"
 #define BLYNK_DEVICE_NAME "ESP8266"
 #define BLYNK_AUTH_TOKEN "NpE9rh2CVCOxYIGL-78m3zfKID46Rtvh"
 #define BLYNK_PRINT Serial
 
-/* Fill-in your Template ID (only if using Blynk.Cloud) */
-//#define BLYNK_TEMPLATE_ID   "YourTemplateID"
-
+// Bibliotecas
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 #include <SoftwareSerial.h>
 
 // You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-
 char auth[] = BLYNK_AUTH_TOKEN;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-
 char ssid[] = "Nicolas";
 char pass[] = "7707224527";
 
-
-// Hardware Serial on Mega, Leonardo, Micro...
 #define EspSerial Serial1
 
-// or Software Serial on Uno, Nano...
-// #include <SoftwareSerial.h>
 //SoftwareSerial EspSerial(2, 3); // RX, TX
-
 SoftwareSerial EspSerial(11, 10); // RX, TX
+
 // Your ESP8266 baud rate:
 #define ESP8266_BAUD 9600
+
 ESP8266 wifi(&EspSerial);
+
+// Configurações dos Motores
 #define pwm 3 /* Transistor */
-int max_range = 200;
+const int max_range = 200; /* Valor máximo da velocidade */
 
 void setup(){
-  pinMode(pwm, OUTPUT);
+  pinMode(pwm, OUTPUT);-
+
   // Debug console
   Serial.begin(9600);
   
@@ -48,12 +43,17 @@ void setup(){
   Blynk.begin(auth, wifi, ssid, pass);
 }
 
-void loop()
-{
+void loop(){
   Blynk.run();
 }
-  BLYNK_WRITE(V1){               //pino virtual selecionado no joystick
+
+//Pino Virtual Selecionado no Joystick
+BLYNK_WRITE(V1){ /* Joystick (V1), Button (V2) */
   int y = param[0].asInt();
+  
+  // Teste
   Serial.print(y);
+
+  // Função
   analogWrite(pwm, y);
 }
