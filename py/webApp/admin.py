9e279878f80fd05ce01e2eeb2865ecdb7817fc02
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2022-07-07 08:46:54 -0300

    .

---
+++ py/webApp/admin.py
@@ -0,0 +1,4 @@
+from learning_logs.models import Topic, Entry # Importa meu modelo Topic
+
+admin.site.register(Topic) # Administra o código por meio do site de administração
+admin.site.register(Entry)
