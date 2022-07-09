369c42b6d72f32b3f1d882d836805e990e589ba0
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2022-07-09 02:38:32 -0300

    DEU CERTO CARAIIOOOOO

--- py/web_book/learning_logs/models.py
+++ py/web_book/learning_logs/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Topic(models.Model):
-    text = models.CharField(max_length=200) # CharField é um dado composto de caracteres (pequena quantidade. Ex: Nome)
     # Max_length -> Está definindo um espaço no banco de dados
+    text = models.CharField(max_length=200) # CharField é um dado composto de caracteres (pequena quantidade. Ex: Nome)
+    
     date_added = models.DateTimeField(auto_now_add=True) # DataTimeField é um dado que registra data e hora
     # auto_now_add=True Django definirá automaticamente com data e hora
 
@@ -11,15 +12,14 @@ class Topic(models.Model):
         return self.text # Neste caso, text
 
 class Entry(models.Model): 
-    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
-    """
     # Instância do Foreign
     # ForeignKey (Chave Estrangeira) se refere a outro registro do banco de dados
     # Cada assunto recebe um ID para podermos relacioná-los depois
     # Xadrez = 1, Livro = 2
     # Quando o Django quiser estabelecer uma conexão entre os dois, ele vai chamar pela chave
-    # """
+    # topic = models.ForeignKey(Topic, )
     
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add = True)
 
--- py/web_book/learning_logs/views.py
+++ py/web_book/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 
 # Create your views here
 
@@ -7,23 +7,32 @@ def index(request):
     # Pagina inicial de Leaning Log
     return render(request, 'learning_logs/index.html')
 
+# A função exige um parâmetro: o objeto request recebido do servidor
 def topics(request): # Mostra todos os assuntos
-    # A função exige um parâmetro: o objeto request recebido do servidor
     
     # Então consultamos o banco de dados solicitando objetos topic com 
-    # atributo date_added
-    # armazenamos o queryset resultante em topics
-    topics = Topic.objects.order_by('date_added')
-    
+        # atributo date_added
+        # armazenamos o queryset resultante em topics
+    topic = Topic.objects.order_by('date_added')
+
     # Definimos o contexto enviado ao template
     # Contexto é um dicionário em que:
 
-    # chaves = nomes que usaremos no template para acessar dados;
-    # valores = dados que devemos enviar ao template.
-    
-    # neste caso há apenas um par chave e valor, que são os assuntos
-    context = {'topics' : topics}
+        # chaves = nomes que usaremos no template para acessar dados;
+        # valores = dados que devemos enviar ao template.
+        # neste caso há apenas um par chave e valor, que são os assuntos
+    context = {'topic': topic}
 
     # Ao construir um página que use dados, passamos context ao Render, além do objeto
     # request e path do template
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id): # Mostra um assunto e todas as suas entradas
+
+    topics = Topic.objects.get(id=topic_id)
+
+    #-date_added exibe a data da Mais Recente para a Menos Recente
+    entries = topic.entry_set.order_by("-date_added")
+    context = {'topics' : topics, 'entries' : entries}
+    
     return render(request, 'learning_logs/topics.html', context)
