58113e69c39be9cc42b4f28d178c0b49cd686d56
nicolas <nicolasauersvalt@gmail.com>
2022-07-15 09:38:52 -0300

    New_Site

---
+++ py/webApp/web_site/views.py
@@ -0,0 +1,14 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import Topic, Entry
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'web_site/index.html')
+
+def topic(request):
+    topic = Topic.objects.order_by('date_added')
+    context = {'topic':topic}
+
+    return render(request, 'web_site/index.html', context)
