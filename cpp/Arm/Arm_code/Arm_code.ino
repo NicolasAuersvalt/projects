852f1110c43f4317ec740559e0cf4088932b8ea7
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2022-08-14 18:13:09 -0300

    Update Arm_code.ino

--- cpp/Arm/Arm_code/Arm_code.ino
+++ cpp/Arm/Arm_code/Arm_code.ino
@@ -1,82 +1,77 @@
 #include <Servo.h>
-#include <ESP8266_Lib.h>
-#include <BlynkSimpleShieldEsp8266.h>
-#include <SoftwareSerial.h>
 
-#define BLYNK_TEMPLATE_ID "TMPL9u3ogMD9"
-#define BLYNK_DEVICE_NAME "Arm"
-#define BLYNK_AUTH_TOKEN "IVPfVD2Jh9oTBzIbc594SNN4V5-WdQws"
+// Micro Servos
+Servo left;
+Servo right;
+Servo bottom; // Base Servo 
+Servo top; // Claw Servo
 
-char auth[] = BLYNK_AUTH_TOKEN;
+// Components
+#define axis_y A2
+#define axis_x A3
+#define button 6
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "Nicolas";
-char pass[] = "7707224527";
+int x;
+int y;
 
-#define EspSerial Serial1
+const int tmp = 1000;
 
-//SoftwareSerial EspSerial(2, 3); RX, TX
-SoftwareSerial EspSerial(10, 11); // RX, TX
-
-// Your ESP8266 baud rate:
-#define ESP8266_BAUD 9600
-
-ESP8266 wifi(&EspSerial);
-
-#define servo_l 2 // Left
-#define servo_c 3 // Claw
-#define servo_r 4 // Right
-#define servo_b 5 // Bottom
-
-Servo l; // Left Servo Object
-Servo c; // Claw Servo Object
-Servo r; // Right Servo Object
-Servo b; // Bottom Servo Object
-  
 void setup(){
-  EspSerial.begin(ESP8266_BAUD);
 
-  Blynk.begin(auth, wifi, ssid, pass);
-  
   Serial.begin(9600);
-  l.attach(servo_l);
-  c.attach(servo_c);
-  r.attach(servo_r);
-  b.attach(servo_b);
-  l.write(0); // Inicia na posição 0
-  r.write(180);
-  b.write(90);
-  delay(500);
+
+  // Pins
+  left.attach(2);
+  right.attach(3);
+  bottom.attach(4);
+
+  // Joystick
+  pinMode(axis_x, INPUT);
+  pinMode(axis_y, INPUT);
+  pinMode(button, INPUT);
 }
 
 void loop(){
-  Blynk.run();
+  
+  int angle = analogRead(0);
+  int angle1= analogRead(1);
+  valbase = map(analogRead(axis_x), 0, 1023, 1, 180);
+  
+  angle=map(angle, 0, 1023, 20, 73); // altura
+  angle1=map(angle1, 0, 1023, 20, 120); // profundidade
+  
+  Serial.println("Profundidade: ");
+  Serial.println(angle);
+  Serial.println("Altura: ");
+  Serial.println(angle1);
+
+
+  // Contraido  
+  // profundidade 37
+  // altura 20
+  
+  //altura 120
+  //prof 83
+  
+  /*
+  // Esticado
+  left.write(30);
+  right.write(160);
+  
+  delay(5000);
+  //levantado
+  left.write(160);
+  right.write(30);
+  delay(tmp);
+  delay(5000);
+  */
+  
+  bottom.write(valbase);
+  delay(40);
+  left.write(angle);
+  delay(40);
+  right.write(angle1);
+  delay(40);
 }
 
-BLYNK_WRITE(V0){ // ARM
-  int y = param[0].asInt(); // Eixo Y
-  Serial.print(y);
-  l.write(y);
-  r.write(180 - y); 
-}
-BLYNK_WRITE(V1){ // Base
-  int x = param[1].asInt(); // Eixo X
-  Serial.print(x);
-  b.write(x);
-}
-BLYNK_WRITE(V2){ // Claw
-  int l = param.asInt(); // Button
-  Serial.print(l);
 
-  if(l == 1){ // Pressionado
-    for(int ang=0; ang <180; ang += 5){
-    c.write(ang);
-  }
-  if(l == 0){ // Solto
-    for(int ang=0; ang <=180; ang -= 5){
-  c.write(ang);
-  }
-  }
-  }
-}
